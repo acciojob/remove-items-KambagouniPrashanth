5ef086f2af073fd79c6cd22c958847ea2bd5cd3a
Kambagouni Prashanth <117985368+KambagouniPrashanth@users.noreply.github.com>
2023-06-30 07:05:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+function remove(){
+    const element=document.getElementById("colorSelect")
+    // console.log(element.selectedIndex)
+    element.remove(element.selectedIndex);
+} 
+   
+
+
+
